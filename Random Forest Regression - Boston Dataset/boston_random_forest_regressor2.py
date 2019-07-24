e791d8b026c1b8c73a04ec824c7b8b89a784c888
Bharathiraja <bharathirajatut@gmail.com>
2019-07-24 13:03:28 +0530

    Update boston_random_forest_regressor2.py

--- Random Forest Regression - Boston Dataset/boston_random_forest_regressor2.py
+++ Random Forest Regression - Boston Dataset/boston_random_forest_regressor2.py
@@ -41,7 +41,8 @@ x = dataset.drop('PRICE', axis = 1)
 y = dataset['PRICE']
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+#from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state = 0)
 
 
